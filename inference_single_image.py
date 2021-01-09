d0976ae824da6102fe2b0027244b5fc94323d402
shantanu <shantanusingh10@gmail.com>
2021-01-09 10:45:27 +0530

    Modified inference code to work on folder of images

--- inference_single_image.py
+++ inference_single_image.py
@@ -21,10 +21,12 @@ from layers import disp_to_depth
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Inference on one Single Image.')
-    parser.add_argument('--image_path', type=str,
-                        default='./asserts/sample.png',
-                        help='path to a test image')
+    parser = argparse.ArgumentParser(description='Inference on images in a directory.')
+    parser.add_argument('--output_dir', type=str,
+                        default='./outputs',
+                        help='path to output directory')
+    parser.add_argument('--images_dir', type=str, default='',
+                        help='path to directory of images')
     parser.add_argument('--model_name', type=str,
                         default='weights_5f',
                         help='name of a pretrained model to use',
@@ -36,6 +38,7 @@ def parse_args():
                         action='store_true')
 
     return parser.parse_args()
+
 
 def prepare_model_for_test(args, device):
     model_path = args.model_name
@@ -61,7 +64,6 @@ def prepare_model_for_test(args, device):
     return encoder, decoder, encoder_dict['height'], encoder_dict['width']
 
 
-
 def inference(args):
     if torch.cuda.is_available() and not args.no_cuda:
         device = torch.device("cuda")
@@ -69,42 +71,52 @@ def inference(args):
         device = torch.device("cpu")
     
     encoder, decoder, thisH, thisW = prepare_model_for_test(args, device)
-    image_path = args.image_path
-    print("-> Inferencing on image ", image_path)
+
+    if os.path.isdir(args.images_dir):
+        image_paths = [os.path.join(args.images_dir, filename) for filename in os.listdir(args.images_dir)
+                       if os.path.splitext(filename)[1] in ['.jpg', '.png']]
+    else:
+        raise Exception("Invalid image directory path: {}".format(args.images_dir))
+
+    output_dir = args.output_dir
+    os.makedirs(output_dir, exist_ok=True)
 
     with torch.no_grad():
-        # Load image and preprocess
-        input_image = pil.open(image_path).convert('RGB')
-        extension = image_path.split('.')[-1]
-        original_width, original_height = input_image.size
-        input_image = input_image.resize((thisH, thisW), pil.LANCZOS)
-        input_image = transforms.ToTensor()(input_image).unsqueeze(0)
+        for image_path in image_paths:
+            # Load image and preprocess
+            input_image = pil.open(image_path).convert('RGB')
+            extension = image_path.split('.')[-1]
+            original_width, original_height = input_image.size
+            input_image = input_image.resize((thisH, thisW), pil.LANCZOS)
+            input_image = transforms.ToTensor()(input_image).unsqueeze(0)
 
-        # PREDICTION
-        input_image = input_image.to(device)
-        outputs = decoder(encoder(input_image))
+            # PREDICTION
+            input_image = input_image.to(device)
+            outputs = decoder(encoder(input_image))
 
-        disp = outputs[("disp", 0)]
-        disp_resized = torch.nn.functional.interpolate(
-            disp, (original_height, original_width), mode="bilinear", align_corners=False)
+            disp = outputs[("disp", 0)]
+            disp_resized = torch.nn.functional.interpolate(
+                disp, (original_height, original_width), mode="bilinear", align_corners=False)
 
-        # Saving numpy file
-        name_dest_npy = image_path.replace('.'+extension, '_depth.npy') 
-        print("-> Saving depth npy to ", name_dest_npy)
-        scaled_disp, _ = disp_to_depth(disp, 0.1, 10)
-        np.save(name_dest_npy, scaled_disp.cpu().numpy())
+            filename = os.path.basename(image_path)
 
-        # Saving colormapped depth image
-        disp_resized_np = disp_resized.squeeze().cpu().numpy()
-        vmax = np.percentile(disp_resized_np, 95)
-        normalizer = mpl.colors.Normalize(vmin=disp_resized_np.min(), vmax=vmax)
-        mapper = cm.ScalarMappable(norm=normalizer, cmap='magma')
-        colormapped_im = (mapper.to_rgba(disp_resized_np)[:, :, :3] * 255).astype(np.uint8)
-        im = pil.fromarray(colormapped_im)
+            # Saving numpy file
+            name_dest_npy = os.path.join(output_dir, filename.replace('.'+extension, '_depth.npy'))
+            print("-> Saving depth npy to ", name_dest_npy)
+            scaled_disp, _ = disp_to_depth(disp, 0.1, 10)
+            np.save(name_dest_npy, scaled_disp.cpu().numpy())
 
-        name_dest_im = image_path.replace('.'+extension, '_depth.png')
-        print("-> Saving depth png to ", name_dest_im)
-        im.save(name_dest_im)
+            # Saving colormapped depth image
+            disp_resized_np = disp_resized.squeeze().cpu().numpy()
+            vmax = np.percentile(disp_resized_np, 95)
+            normalizer = mpl.colors.Normalize(vmin=disp_resized_np.min(), vmax=vmax)
+            mapper = cm.ScalarMappable(norm=normalizer, cmap='magma')
+            colormapped_im = (mapper.to_rgba(disp_resized_np)[:, :, :3] * 255).astype(np.uint8)
+            im = pil.fromarray(colormapped_im)
+
+            name_dest_im = os.path.join(output_dir, filename.replace('.'+extension, '_depth.png'))
+            print("-> Saving depth png to ", name_dest_im)
+            im.save(name_dest_im)
 
     print('-> Done!')
 
